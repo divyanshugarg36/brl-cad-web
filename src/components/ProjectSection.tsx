30026ceef4924d4002b633d5e7228968eea05c77
Divyanshu Garg <divyanshugarg36@gmail.com>
2022-07-17 02:00:02 +0530

    refactor: add new project list
    - move text to i18n

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -5,38 +5,21 @@ import Link from 'next/link';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import projects from '../projects.json';
 import { Button, ProjectCard } from './common';
 
 interface IProps { }
 
 export const ProjectSection: React.FC<IProps> = () => {
   const { t } = useTranslation();
-
-  const projects = [
-    {
-      title: t('ABOUT.ABOUT_1.TITLE'),
-      description: t('ABOUT.ABOUT_1.DESCRIPTION'),
-    },
-    {
-      title: t('ABOUT.ABOUT_2.TITLE'),
-      description: t('ABOUT.ABOUT_2.DESCRIPTION'),
-    },
-  ];
-
   return (
     <section className="project-section">
       <div className="project-main">
         <Image className="project-main-image" src="https://github.com/BRL-CAD.png" height={500} width={500} layout="fixed" />
         <div className="project-main-body">
-          <h1>Our Projects</h1>
+          <h1>{t('PROJECT.TITLE')}</h1>
           <p>
-            BRL-CAD is a powerful cross-platform open source combinatorial
-            solid modeling system that includes an interactive 3D solid geometry
-            editor, a network-distributed symmetric multiprocessing (SMP)
-            high-performance ray-tracer with support for rendering and geometric
-            analysis, image and signal-processing tools, a system performance
-            analysis benchmark suite, a flexible geometry scripting interface,
-            and a high-performance geometric representation and analysis library.
+            {t('PROJECT.DESCRIPTION')}
           </p>
           <Link href={LINKS.GITHUB}>
             <a>
@@ -46,11 +29,22 @@ export const ProjectSection: React.FC<IProps> = () => {
         </div>
       </div>
       <div className="project-section-list">
-        {projects.map(({ title, description }) => (
-          <ProjectCard title={title} description={description} />
+        {projects.slice(0, 2).map(({ title, description, link }) => (
+          <ProjectCard
+            key={title}
+            title={title}
+            description={description}
+            link={link}
+          />
         ))}
       </div>
-      <Button className="brl-button--small brl-button--reverse">View All</Button>
+      <Button
+        elementType="a"
+        className="brl-button--small brl-button--reverse"
+        href="/projects"
+      >
+        {t('PROJECT.BUTTON')}
+      </Button>
     </section>
   );
 };
--- src/components/common/ProjectCard.tsx
+++ src/components/common/ProjectCard.tsx
@@ -1,26 +1,34 @@
 import { classNames } from '@constants';
 import { GithubIcon } from '@icons';
+import Link from 'next/link';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 interface IProps {
   className?: string
   title: string;
   description: string;
+  link: string;
   isImage?: boolean;
 }
 
 export const ProjectCard: React.FC<IProps> = ({
-  className, title, description, isImage,
-}) => (
-  <div className={classNames('project-card', className)}>
-    <div className="project-card-body">
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <a>Visit Github</a>
+  className, title, description, link, isImage,
+}) => {
+  const { t } = useTranslation();
+  return (
+    <div className={classNames('project-card', className)}>
+      <div className="project-card-body">
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <Link href={link}>
+          <a target="_blank">{t('PROJECT.VIEW_ALL')}</a>
+        </Link>
+      </div>
+      {isImage && <GithubIcon />}
     </div>
-    {isImage && <GithubIcon />}
-  </div>
-);
+  );
+};
 
 ProjectCard.defaultProps = {
   className: '',
