8eb462ab0b87ae52307a8264e841d697babe6c2e
Divyanshu Garg <divyanshugarg36@gmail.com>
2022-07-17 02:36:40 +0530

    feat: add project page using project cards
    - add new page title component

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,4 +4,5 @@ export { Button } from './Button';
 export { FeatureBlock } from './FeatureBlock';
 export { HighlightBlock } from './HighlightBlock';
 export { Modal } from './Modal';
+export { PageHeader } from './PageHeader';
 export { ProjectCard } from './ProjectCard';
